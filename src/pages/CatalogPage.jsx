e3337c670c3545409bf10dbdb8025d25b9b37882
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-19 15:26:01 +0300

    Update CatalogPage.jsx

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCarsOperation } from 'redux/cars/operations';
-import { selectCars } from 'redux/cars/selectors';
+import CatalogLayout from 'components/CatalogPage/CatalogLayout';
+import Container from 'components/Container/Container';
 
 const CatalogPage = () => {
-  const dispatch = useDispatch();
-  const carsData = useSelector(selectCars);
-  useEffect(() => {
-    (async () => {
-      await dispatch(getCarsOperation());
-    })();
-  }, [dispatch]);
-
-  const onLoadMore = async () => {
-    await dispatch(getCarsOperation());
-  };
-
-  console.log(carsData);
-
   return (
-    <div>
-      CatalogPage <button onClick={onLoadMore}>Load More</button>
-    </div>
+    <Container>
+      <CatalogLayout />
+    </Container>
   );
 };
 
