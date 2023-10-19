c29090de987a191e327a179f21d211ac2cac4ca8
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-19 15:25:03 +0300

    add card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ButtonText,
   CardButton,
   CardStyled,
   CardTitle,
   ContainerImg,
+  Cost,
   IconFavorite,
   Item,
   ItemText,
   List,
   MainImg,
+  Make,
+  Model,
+  Year,
 } from './Card.styled';
 
 import sprite from '../../assets/images/icons/icons.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteFavoriteCard, pushFavoriteCard } from 'redux/cars/carsSlice';
+import { selectFavorites } from 'redux/cars/selectors';
+import { json } from 'react-router-dom';
 
-const Card = () => {
+const Card = ({ data }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const favoriteCards = useSelector(selectFavorites);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    favoriteCards.find(({ id }) => id === data.id && setIsFavorite(true));
+  }, [data.id, favoriteCards]);
+
+  // const {
+  //   id,
+  //   year,
+  //   make,
+  //   model,
+  //   type,
+  //   img,
+  //   description,
+  //   fuelConsumption,
+  //   engineSize,
+  //   accessories,
+  //   functionalities,
+  //   rentalPrice,
+  //   rentalCompany,
+  //   address,
+  //   rentalConditions,
+  //   mileage,
+  // } = data;
+  const formatYear = year => {
+    const res = year.split('-')[0];
+    return res;
+  };
+
+  const formatCost = cost => {
+    const res = cost.split('.')[0];
+    return res;
+  };
+
+  const onFavoriteAdd = data => {
+    dispatch(pushFavoriteCard(data));
+  };
+
+  const onFavoriteDelete = id => {
+    dispatch(deleteFavoriteCard(id));
+    setIsFavorite(false);
+  };
+
+  const addDotText = (letter, text, before) => {
+    const textArray = text.split('');
+    const beforeText = before.split('');
+    if (beforeText.length > 8) {
+      letter = 2;
+    }
+    if (textArray.length > letter) {
+      let formatText = textArray.slice(0, letter).join('');
+
+      return (formatText += '...');
+    }
+    return text;
+  };
+
   return (
     <CardStyled>
       <ContainerImg>
-        <MainImg src="" alt="" />
-        <IconFavorite>
-          <use href={`${sprite}#icon-normal-favorite`}></use>
-        </IconFavorite>
+        <MainImg src={data?.img} alt="" />
+        {isFavorite ? (
+          <IconFavorite active onClick={() => onFavoriteDelete(data.id)}>
+            <use xlinkHref={`${sprite}#icon-active-favorite`}></use>
+          </IconFavorite>
+        ) : (
+          <IconFavorite onClick={() => onFavoriteAdd(data)}>
+            <use xlinkHref={`${sprite}#icon-normal-favorite`}></use>
+          </IconFavorite>
+        )}
       </ContainerImg>
-      <CardTitle></CardTitle>
+      <CardTitle>
+        <Make>{data?.make}</Make>
+        <Model>{` ${addDotText(9, data?.model, data?.make)}, `}</Model>
+        <Year>{formatYear(data?.year)}</Year>
+        <Cost>{`$${formatCost(data?.rentalPrice)}`}</Cost>
+      </CardTitle>
       <List>
         <Item>
           <ItemText></ItemText>
--- src/components/Card/Card.styled.jsx
+++ src/components/Card/Card.styled.jsx
@@ -6,32 +6,63 @@ export const ContainerImg = styled.div`
   position: relative;
   border-radius: 14px;
   height: 268px;
+  border-radius: 14px;
+  overflow: hidden;
+  margin-bottom: 14px;
 `;
 
-export const MainImg = styled.img``;
+export const MainImg = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
 
 export const IconFavorite = styled.svg`
+  cursor: pointer;
   position: absolute;
-  width: 24px;
-  height: 24px;
-  top: 0;
-  right: 0;
-  fill: transparent;
-  & use {
-    stroke: red;
-  }
+  width: 20px;
+  height: 20px;
+  top: 14px;
+  right: 14px;
+  fill: ${({ active }) =>
+    active ? 'var(--brand-color);' : 'var(--button-color-text);'};
 `;
 
-export const CardTitle = styled.h2``;
+export const CardTitle = styled.h2`
+  display: flex;
+  gap: 4px;
+  margin-bottom: 8px;
+  font-size: 16px;
+  line-height: 24px;
+`;
 
-export const List = styled.ul``;
+export const Make = styled.span`
+  white-space: nowrap;
+`;
+export const Model = styled.span`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: var(--brand-color);
+`;
+export const Year = styled.span`
+  /* display: inline-block; */
+  margin-right: auto;
+`;
+export const Cost = styled.span`
+  margin-left: 20px;
+`;
+
+export const List = styled.ul`
+  margin-bottom: 28px;
+`;
 
 export const Item = styled.li``;
 
 export const ItemText = styled.span``;
 
 export const CardButton = styled.button`
-  padding: 12px 99px;
+  width: 100%;
+  padding: 12px;
   border-radius: 12px;
   border: none;
   background-color: var(--brand-color);
