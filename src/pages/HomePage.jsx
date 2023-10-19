73f7dbf180f7abb1d9ab7bee0d4fa36e9a38c909
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-19 15:25:56 +0300

    add pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,7 @@
-import Card from 'components/Card/Card';
 import React from 'react';
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-      <Card />
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
